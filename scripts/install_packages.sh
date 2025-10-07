6894c319b11c94b8c8245d0cee1488084ee6f778
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-10-07 13:50:19 +0600

    Update install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 # Linux setup.
-apt update -y && apt upgrade -y
-apt install -y p7zip-full lz4 android-sdk-libsparse-utils wget python3 python3-pip
+
+sudo apt update && \
+DEBIAN_FRONTEND=noninteractive sudo apt install -yq \
+  attr ccache clang ffmpeg golang \
+  libbrotli-dev libgtest-dev libprotobuf-dev libunwind-dev libpcre2-dev \
+  libzstd-dev linux-modules-extra-$(uname -r) lld protobuf-compiler webp \
+  p7zip-full lz4 android-sdk-libsparse-utils wget python3 python3-pip && \
+sudo modprobe erofs f2fs
 
 # Installing Python packages. (silent)
 pip3 install liblp google-api-python-client google-auth-httplib2 google-auth-oauthlib tgcrypto pyrogram
