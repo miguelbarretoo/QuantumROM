a4aa6c308beb3c9ac156016e7ec995d2707cd27f
Abdullah Al Noman <125588871+SN-Abdullah-Al-Noman@users.noreply.github.com>
2025-10-07 09:50:39 +0600

    Update mod_rom_1.sh

--- mod_rom_1.sh
+++ mod_rom_1.sh
@@ -14,37 +14,45 @@ WORK_DIR="work"
 OUT_DIR="out"
 GIT_SUPPORT_SIZE=2147483648
 
+echo ""
 # --- Setup Directories ---
 chmod +x ./scripts/setup_directories.sh
 bash ./scripts/setup_directories.sh "$FW_DIR" "$WORK_DIR" "$OUT_DIR"
 
+echo ""
 echo "--- Downloading $MODEL $CSC firmware ---"
 chmod +x ./scripts/download_firmware.sh
 bash ./scripts/download_firmware.sh "$MODEL" "$CSC" "$IMEI" "$FW_DIR" "$MODEL"
 
+echo ""
 echo "--- Extracting Firmware ---"
 chmod +x ./scripts/extract_firmware.sh
 bash ./scripts/extract_firmware.sh "$(pwd)/${FW_DIR}/${MODEL}" "${MODEL}.zip"
 
+echo ""
 echo "--- Unpacking images ---"
 chmod +x ./scripts/extract_ext4.sh
 bash ./scripts/extract_ext4.sh "$(pwd)/${FW_DIR}/${MODEL}"
 
+echo ""
 echo "--- Debloating ---"
 chmod +x ./QuantumROM/mods/debloater.sh
 bash ./QuantumROM/mods/debloater.sh "$(pwd)/${FW_DIR}/${MODEL}"
 
+echo ""
 echo "--- Disabling Security ---"
 chmod +x ./QuantumROM/mods/security_disabler.sh
 chmod +x ./QuantumROM/mods/musti_disabler.sh
 bash ./QuantumROM/mods/security_disabler.sh "$(pwd)/${FW_DIR}/${MODEL}"
 bash ./QuantumROM/mods/musti_disabler.sh "$(pwd)/${FW_DIR}/${MODEL}"
 
+echo ""
 echo "--- Packing .img ---"
 chmod +x ./bin/make_ext4fs
 chmod +x ./scripts/pack_ext4.sh
 bash ./scripts/pack_ext4.sh "$(pwd)/${FW_DIR}/${MODEL}" "$(pwd)/${BIN_DIR}" "$(pwd)/${OUT_DIR}"
 
+echo ""
 echo "--- Splitting files if needed ---"
 for file in "$OUT_DIR"/*; do
   [ -f "$file" ] || continue
