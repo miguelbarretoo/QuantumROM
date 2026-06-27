1752f606b32d8c0ee26a55a4cdb3ac1247050ec6
miguelbarretoo <miguel03barreto@gmail.com>
2026-06-27 19:46:27 -0300

    SM-G980F:init

---
+++ sixteen.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+if [ "$#" -lt 6 ]; then
+    echo "Usage: $0 <STOCK_DEVICE> <USE_UI_8_TETHERING_APEX> <TARGET_DEVICE> <TARGET_DEVICE_CSC> <TARGET_DEVICE_IMEI> <OUTPUT_FILESYSTEM>"
+    exit 1
+fi
+
+# Device info
+export STOCK_DEVICE="$1"
+export USE_UI_8_TETHERING_APEX="$2"
+export TARGET_DEVICE="$3"
+export TARGET_DEVICE_CSC="$4"
+export TARGET_DEVICE_IMEI="$5"
+export OUTPUT_FILESYSTEM="$6"
+
+VERSION="1"
+
+# Directories
+export FIRM_DIR="$(pwd)/FW"
+export OUT_DIR="$(pwd)/OUT"
+export WORK_DIR="$(pwd)/WORK"
+export APKTOOL="$(pwd)/bin/java/apktool.jar"
+export DEVICES_DIR="$(pwd)/QuantumROM/Devices"
+export VNDKS_COLLECTION="$(pwd)/QuantumROM/vndks"
+export BUILD_PARTITIONS="product,system_ext,system"
+
+# Source
+source "$(pwd)/scripts/debloat.sh"
+source "$(pwd)/scripts/QuantumRom.sh"
+
+#EXTRACT_FIRMWARE "$FIRM_DIR/$TARGET_DEVICE"
+EXTRACT_SUPER_IMG "$FIRM_DIR/$TARGET_DEVICE"
+EXTRACT_FIRMWARE_IMG "$FIRM_DIR/$TARGET_DEVICE" "all"
+
+DECODE_OMC "$FIRM_DIR/$TARGET_DEVICE" "$WORK_DIR"
+DEBLOAT "$FIRM_DIR/$TARGET_DEVICE"
+
+APPLY_STOCK_CONFIG "$FIRM_DIR/$TARGET_DEVICE"
+PATCH_SELINUX "$FIRM_DIR/$TARGET_DEVICE"
+DISABLE_SECURITY "$FIRM_DIR/$TARGET_DEVICE"
+ADD_SAMSUNG_FLAGSHIP_APPS "$FIRM_DIR/$TARGET_DEVICE"
+APPLY_CUSTOM_FEATURES "$FIRM_DIR/$TARGET_DEVICE"
+
+INSTALL_FRAMEWORK "$APKTOOL" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework/framework-res.apk"
+
+DECOMPILE "$APKTOOL" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework/ssrm.jar" "$WORK_DIR"
+DECOMPILE "$APKTOOL" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework/services.jar" "$WORK_DIR"
+DECOMPILE "$APKTOOL" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework/samsungkeystoreutils.jar" "$WORK_DIR"
+
+PATCH_SSRM "$WORK_DIR/ssrm"
+PATCH_FLAG_SECURE "$WORK_DIR/services"
+PATCH_SECURE_FOLDER "$WORK_DIR/services"
+PATCH_PRIVATE_SHARE "$WORK_DIR/samsungkeystoreutils"
+
+RECOMPILE "$APKTOOL" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework" "$WORK_DIR/ssrm" "$WORK_DIR"
+RECOMPILE "$APKTOOL" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework" "$WORK_DIR/services" "$WORK_DIR"
+RECOMPILE "$APKTOOL" "$FIRM_DIR/$TARGET_DEVICE/system/system/framework" "$WORK_DIR/samsungkeystoreutils" "$WORK_DIR"
+mv -f "$WORK_DIR"/*.jar "$FIRM_DIR/$TARGET_DEVICE/system/system/framework/"
+
+PATCH_BT_LIB "$FIRM_DIR/$TARGET_DEVICE" "$WORK_DIR"
+
+B_ID="$(grep -m1 '^ro.system.build.id=' "$FIRM_DIR/$TARGET_DEVICE/system/system/build.prop" | cut -d= -f2 | tr -d '\r')"
+B_V="$(grep -m1 '^ro.system.build.version.incremental=' "$FIRM_DIR/$TARGET_DEVICE/system/system/build.prop" | cut -d= -f2 | tr -d '\r')"
+BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "system" "ro.build.display.id" "${B_ID} ${B_V} V-${VERSION}: Built with Quantum Tools"
+BUILD_PROP "$FIRM_DIR/$TARGET_DEVICE" "product" "ro.build.display.id" "${B_ID} ${B_V} V-${VERSION}: Built with Quantum Tools"
+
+BUILD_IMG "$FIRM_DIR/$TARGET_DEVICE" "all" "$OUTPUT_FILESYSTEM" "$OUT_DIR"
